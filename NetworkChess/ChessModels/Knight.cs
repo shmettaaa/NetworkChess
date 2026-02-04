d55e4c91666c0a61c23a30c027cfdc50d20de0f7
shmettaaa <yavayhyh@gmail.com>
2026-02-04 23:36:24 +0300

    Add project files.

---
+++ NetworkChess/ChessModels/Knight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkChess.ChessModels
+{
+    internal class Knight
+    {
+    }
+}
---
+++ NetworkChess/ChessModels/Piece.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkChess.ChessModels
+{
+
+
+    internal abstract class Piece
+    {
+
+        public Position BoardPosition
+        {
+            get;
+            set;
+        }
+        public PieceColor Color
+        {
+            get;
+        }
+
+        protected Piece(Position pos, PieceColor color)
+        {
+            BoardPosition = pos;
+            Color = color ;
+        }
+    }
+
+
+
+
+    struct Position
+    {
+      public int Row
+        {
+            get;
+            set;
+        }
+        public int Col
+        {
+            get;
+            set;
+        }
+    }
+
+
+
+    public enum PieceColor
+    {
+        White,
+        Black
+    }
+}
